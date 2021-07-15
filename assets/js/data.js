b914e7f894343b087789863248b244a677c4bb9c
samuel do prado almeida <samuelprado.a@gmail.com>
2021-07-15 19:30:57 -0300

    Update data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,49 +1,62 @@
-var client = {
-    add: function() {
-        //event.keyCode === 13
-        let btn_add_client = document.querySelector('button#add_client');
-        let input_client_keyCode = document.querySelector('input#input_client');
+const timesheet = {
 
-        input_client_keyCode.addEventListener("keyup", function(event) {
-            if (event.keyCode === 13) {
-                client.send();
+    /*
+    |--------------------------------------------------------------------------
+    | Clients
+    |--------------------------------------------------------------------------
+    */
+    client: {
+        add: function() {
+
+            let btn_add_client = document.querySelector('button#add_client');
+            let input_client_keyCode = document.querySelector('input#input_client');
+
+            input_client_keyCode.addEventListener("keyup", function(event) {
+                if (event.keyCode === 13) {
+                    timesheet.client.send();
+                }
+            });
+
+            btn_add_client.addEventListener('click', timesheet.client.send);
+        },
+        all: function() {
+            return JSON.parse(timesheet.server.all('clients'))
+        },
+        send: function() {
+
+            let input_client = document.querySelector('input#input_client');
+            let clients = [];
+
+            if (input_client.value.length < 1) {
+                alert('Please enter a client name');
             }
-        });
 
-        btn_add_client.addEventListener('click', client.send);
-    },
-    all: function() {
-        return JSON.parse(server.all('clients'))
-    },
-    send: function() {
+            if (timesheet.server.all('clients')) {
+                clients = JSON.parse(timesheet.server.all('clients'));
+            }
 
-        let input_client = document.querySelector('input#input_client');
-        let clients = [];
+            clients.push({ id: clients.length, name: input_client.value });
+            timesheet.server.insert('clients', clients);
 
-        if (input_client.value.length < 1) {
-            alert('Please enter a client name');
+            timesheet.views.clientListTable();
+            console.log(clients);
+            input_client.value = '';
         }
+    },
 
-        if (server.all('clients')) {
-            clients = JSON.parse(server.all('clients'));
-        }
-
-        clients.push({ id: clients.length, name: input_client.value });
-        server.insert('clients', clients);
-
-        views.clientListTable();
-        console.log(clients);
-        input_client.value = '';
-    }
-}
-var views = {
-    clientListTable: function() {
-        let client_view = document.querySelector('#list_clients tbody').innerHTML = '';
-        client_view = document.querySelector('#list_clients tbody');
-        let clients = client.all();
-        if (clients.length != -1) {
-            clients.forEach(function(client, key) {
-                client_view.innerHTML += `
+    /*
+    |--------------------------------------------------------------------------
+    | Views
+    |--------------------------------------------------------------------------
+    */
+    views: {
+        clientListTable: function() {
+            let client_view = document.querySelector('#list_clients tbody').innerHTML = '';
+            client_view = document.querySelector('#list_clients tbody');
+            let clients = timesheet.client.all();
+            if (clients.length != -1) {
+                clients.forEach(function(client, key) {
+                    client_view.innerHTML += `
             <tr id="${client.id}">
                 <td>${client.id}</td>
                 <td>${client.name}</td>
@@ -54,17 +67,23 @@ var views = {
                 </td>
             </tr>
             `;
-            });
+                });
+            }
         }
-    }
-}
-
-var server = {
-    all: function(key) {
-        return localStorage.getItem(key);
     },
-    insert: function(key, param) {
-        localStorage.setItem(key, JSON.stringify(param));
+
+    /*
+    |--------------------------------------------------------------------------
+    | Server
+    |--------------------------------------------------------------------------
+    */
+    server: {
+        all: function(key) {
+            return localStorage.getItem(key);
+        },
+        insert: function(key, param) {
+            localStorage.setItem(key, JSON.stringify(param));
+        }
     }
 }
 
@@ -73,5 +92,5 @@ var server = {
 | Started
 |--------------------------------------------------------------------------
 */
-client.add();
-views.clientListTable();
+timesheet.client.add();
+timesheet.views.clientListTable();
